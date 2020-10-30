1cd92c39c0de2a6ca8b4a5fe7943d561942b577a
TusharPatil010010 <tusharp010010@gmail.com>
2020-10-30 23:47:49 +0530

    Added : added queries to extend employee_payroll table to have multiple pays

--- Payroll Service.sql
+++ Payroll Service.sql
@@ -36,7 +36,7 @@ describe employee_payroll_service;
 
 #UC7:
 insert into employee_payroll_service(name, gender, salary, start) VALUES
-    -> ('Eve', 'F',6000000.00,'2010-01-02');
+    -> ('Terissa', 'F',6000000.00,'2010-01-02');
 select * from employee_payroll_service;
 select gender, AVG(salary) from employee_payroll_service group by gender;
 select gender, SUM(salary) from employee_payroll_service group by gender;
@@ -49,3 +49,11 @@ alter table employee_payroll_service add phone numeric(10) after name;
 alter table employee_payroll_service add address varchar(250) after phone;
 alter table employee_payroll_service add department varchar(150) NOT NULL after address;
 alter table employee_payroll_service alter address SET DEFAULT 'India';
+
+#UC9
+alter table employee_payroll_service RENAME COLUMN salary to basic_pay;
+alter table employee_payroll_service add deductions double NOT NULL after basic_pay;
+alter table employee_payroll_service add taxable_pay double NOT NULL after deductions;
+alter table employee_payroll_service add tax double NOT NULL after taxable_pay;
+alter table employee_payroll_service add net_pay double NOT NULL after tax;
+
